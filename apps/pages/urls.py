7242cecea9c773faf3e47a0b222f1774a400022d
SamArndt <sarndt196@gmail.com>
2026-02-16 11:03:13 -0700

    Added home page with hidden buttons for unauthenticated users, base template, and styling with bootstrap

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,6 +1,8 @@
 from django.views.generic.base import TemplateView
 from django.urls import path
 
+app_name = "pages"
+
 urlpatterns = [
   path("", TemplateView.as_view(template_name="pages/home.html"), name = "home"),
 ]
