0e87ece251673ed44c940eacac77478780c3a403
SamArndt <sarndt196@gmail.com>
2026-02-17 19:25:08 -0700

    Styling for new login page, fixed authenticated buttons spacing

--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,10 @@ DATABASES = {
 
 LOGIN_REDIRECT_URL = "/"
 
+LOGOUT_REDIRECT_URL = ‘/’
+
+LOGIN_URL = 'login'
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
