d4c55ca0fcf731b17c47099f4768627a6ac781d0
scarndt <sarndt196@gmail.com>
2026-02-19 11:38:00 -0700

    Fully implemented signup functionality (#3)

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(AuthenticationForm):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     for field_name, field in self.fields.items():
       field.widget.attrs['class'] = 'form-control my-2'
+
+class SignUpForm(UserCreationForm):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    for field_name, field in self.fields.items():
+      field.widget.attrs['class'] = 'form-control my-2'
+
