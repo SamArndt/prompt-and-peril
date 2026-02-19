f464ea050ce4e7bcca12e69b87953785af62ad65
SamArndt <sarndt196@gmail.com>
2026-02-18 19:30:51 -0700

    Fully implemented signup functionality

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import views as auth_views
 from .forms import LoginForm
 from django.urls import path, include
+from accounts import views
 
 app_name = "accounts"
 
 urlpatterns = [
-  # path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
   path("login/", auth_views.LoginView.as_view(template_name="registration/login.html", authentication_form=LoginForm), name="login"),
   path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-
+  path("signup/", views.SignUpView.as_view(), name="signup"),
 ]
+
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm 
+    """
+    for all generic class-based views, the URLs are not loaded when the file is imported,
+    so we have to use the lazy form of reverse to load them later when we are sure they're available.
+    """
+    success_url = reverse_lazy("accounts:login")
+    template_name = "registration/signup.html"
+
